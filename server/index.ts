8fa0348a05d044018109fe5e51418f713e7f35a4
Hearty, Oh <heartyoh@hatiolab.com>
2020-07-08 19:10:04 +0900

    lazada interface ..ing

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,19 @@
+import { Connections, ScenarioEngine } from '@things-factory/integration-base'
+
 export * from './entities'
 export * from './graphql'
 
 import './middlewares'
 import './routes'
 import './engine'
+
+process.on('bootstrap-module-start' as any, async ({ app, config, client }: any) => {
+  console.log('%%%%%%%%%%%%%%%% TASK ENGINE - BEGIN %%%%%%%%%%%%%%%%')
+  try {
+    await Connections.ready()
+    // await ScenarioEngine.loadAll()
+  } catch (ex) {
+    Connections.logger.error(ex)
+  }
+  console.log('%%%%%%%%%%%%%%%% TASK ENGINE - END %%%%%%%%%%%%%%%%')
+})
