034c5db5a2ac49a7617dc79b5803f32a615cfd9a
horwengliang <horwengliang@gmail.com>
2020-08-11 14:10:46 +0800

    Update product-variation-setting.js

--- client/pages/catalogue/product-variation-setting.js
+++ client/pages/catalogue/product-variation-setting.js
@@ -1,15 +1,15 @@
-import { getCodeByName } from '@things-factory/code-base'
 import { MultiColumnFormStyles } from '@things-factory/form-ui'
 import { i18next, localize } from '@things-factory/i18n-base'
 import { client } from '@things-factory/shell'
 import { gqlBuilder } from '@things-factory/utils'
 import gql from 'graphql-tag'
 import { css, html, LitElement } from 'lit-element'
-import { WizardViewStyles } from '../components/wizard-view-styles'
 
 class ProductVariationSetting extends localize(i18next)(LitElement) {
   static get properties() {
-    return {}
+    return {
+      productInfo: Object
+    }
   }
 
   static get styles() {
@@ -28,11 +28,6 @@ class ProductVariationSetting extends localize(i18next)(LitElement) {
         }
       `
     ]
-  }
-
-  constructor() {
-    super()
-    this.storageTypes = []
   }
 
   get inputForm() {
@@ -71,29 +66,25 @@ class ProductVariationSetting extends localize(i18next)(LitElement) {
     `
   }
 
-  async firstUpdated() {
-    this.storageTypes = await getCodeByName('STORAGE_TYPES')
-  }
-
-  async _createProduct() {
+  async _commit() {
     try {
-      this._validateProductInformation()
+      this._validateVariationInformation()
 
       const result = await CustomAlert({
         title: i18next.t('title.are_you_sure'),
-        text: i18next.t('text.create_new_product'),
+        text: i18next.t('text.create_new_variation_draft'),
         confirmButton: { text: i18next.t('button.confirm') },
         cancelButton: { text: i18next.t('button.cancel') }
       })
       if (!result.value) return
 
-      let marketplaceProduct = this._getFormInfo()
-      const args = marketplaceProduct
+      let marketplaceProductVariation = this._getFormInfo()
+      const args = marketplaceProductVariation
 
       const response = await client.query({
         query: gql`
             mutation ($attachments: Upload) {
-              createMarketplaceProduct(${gqlBuilder.buildArgs(args)}, file:$attachments) {
+              upsertMarketplaceProductVariation(${gqlBuilder.buildArgs(args)}, file:$attachments) {
                 id
                 name
               }
@@ -107,14 +98,16 @@ class ProductVariationSetting extends localize(i18next)(LitElement) {
         }
       })
       if (!response.errors) {
-        this._clearView()
+        this._showToast({ message: i18next.t('text.draft_variation_has_been_created_successfully') })
+        this.dispatch(new CustomEvent('submit', { marketplaceProduct }))
+        return true
       }
     } catch (e) {
       this._showToast(e)
     }
   }
 
-  async _validateProductInformation() {
+  async _validateVariationInformation() {
     if (!this.inputForm.checkValidity()) throw new Error(i18next.t('text.form_is_incomplete'))
   }
 
@@ -127,6 +120,10 @@ class ProductVariationSetting extends localize(i18next)(LitElement) {
     Array.from(form.querySelectorAll('input, select')).forEach(field => {
       if (!field.hasAttribute('hidden') && field.value) {
         obj[field.name] = field.type === 'checkbox' ? field.checked : field.value
+      }
+
+      if (field.type === 'number') {
+        obj[field.name] = parseFloat(field.value)
       }
     })
 
